088570f006a6a0267f681ff7d6edeb9fd5d54402
AnonymousManan <mananaggarwal2001@gmail.com>
2024-05-30 20:43:13 +0530

    Saving the job in the user list done

--- src/main/java/com/mananluvtocode/jobportal/controller/JobSeekerSaveController.java
+++ src/main/java/com/mananluvtocode/jobportal/controller/JobSeekerSaveController.java
@@ -1,5 +1,4 @@
 package com.mananluvtocode.jobportal.controller;
-
 import com.mananluvtocode.jobportal.entity.JobPostActivity;
 import com.mananluvtocode.jobportal.entity.JobSeekerProfile;
 import com.mananluvtocode.jobportal.entity.JobSeekerSave;
@@ -17,7 +16,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.ArrayList;
 import java.util.List;
 
